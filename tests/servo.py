559cc19ac0dbadadae82f8d476547454e351f463
myowngit1213 <cavin061213@outlook.com>
2023-08-04 20:42:23 +0700

    Updated requirements.txt

--- tests/servo.py
+++ tests/servo.py
@@ -4,12 +4,12 @@ from time import sleep
 
 pigpio_factory = PiGPIOFactory()
 servo =AngularServo(7, min_angle=0, max_angle=270, min_pulse_width=0.0005, max_pulse_width=0.0025, pin_factory=pigpio_factory)
-servo.angle = 95
+servo.angle = 180
+sleep(2)
+servo.angle = 90
 sleep(2)
 servo.angle = 135
 sleep(2)
-servo.angle = 50
-sleep(2)
-servo.angle = 95
-sleep(2)
+# servo.angle = 95
+# sleep(2)
 
