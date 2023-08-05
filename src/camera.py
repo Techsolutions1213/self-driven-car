f9319a4975eb2d2d0157c91cb5d12a84ad75e248
myowngit1213 <cavin061213@outlook.com>
2023-08-05 19:42:57 +0700

    Created the lidar class

--- src/camera.py
+++ src/camera.py
@@ -3,14 +3,16 @@ import cv2
 class camera:
     def __init__(self, port=0):
         self.vs = cv2.VideoCapture(port)
+        self.running = True
         self.img = None
 
     def update(self):
-        while True:
+        while self.running:
             _, self.img = self.vs.read()
 
     def run(self):
         return self.img
 
     def shutdown(self):
+        self.running = False
         self.vs.release()
