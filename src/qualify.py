928a7286d05dedc1aae036cca4371eb67fb00a15
myowngit1213 <cavin061213@outlook.com>
2023-08-12 15:22:59 +0700

    Reorganised files

--- src/qualify.py
+++ src/qualify.py
@@ -1,116 +1,17 @@
-import imp
-from camera import camera
-import functions as ft
+from parts.camera import camera
+import utils as ft
 import threading
 import time
 import cv2
 
-# cam = camera()
-# def main():
-#     vs = threading.Thread(target=cam.update, daemon=True)
-#     vs.start()
-#     ft.drive(0, 95, 0)  
-#     time.sleep(1)
-#     print('[INFO] Waiting for the button to be pressed')
-#     while ft.getBtState(): # Not pressed
-#         pass
-#     time.sleep(0.5)
-#     print('[INFO] GO!')
-
-#     timeIt = time.time()
-#     numLines = 0
-#     mode = 0
-#     while numLines < 12:
-#         if not ft.getBtState():
-#             print(f'[INFO] Total time: {time.time() - timeIt:.2f}s')
-#             return
-
-#         # Acquire a frame from the camera
-#         ret, frame = cam.get()
-#         if not ret:
-#             continue
-#         frame = cv2.flip(frame, -1)
-
-#         # Image processing
-#         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-#         crop = hsv[:, 300:340]
-
-#         maskBL = cv2.inRange(crop, ft.lowerBL, ft.upperBL)
-#         maskOR = cv2.inRange(crop, ft.lowerOR, ft.upperOR) 
-
-#         # Contours            
-#         bx, by, bw, bh = ft.findContour(maskBL, 400, '')
-#         ox, oy, ow, oh = ft.findContour(maskOR, 400, '')
-
-#         if False:
-#             continue
-
-#         if mode == 0: # Check line
-#             if by > 300:
-#                 ft.turnTimer(0.8, [80, 95, 1])
-#                 ft.turnTimer(0.8, [80, 115, 1])
-#                 numLines += 1
-#                 print(f'Passed {numLines}, mode = {mode}')
-#                 mode = 1
-#             elif oy > 300:
-#                 ft.turnTimer(0.8, [80, 95, 1])
-#                 ft.turnTimer(0.8, [80, 65, 1])
-#                 numLines += 1
-#                 print(f'Passed {numLines}, mode = {mode}')
-#                 mode = 2
-#             elif not ft.sensorDetect():
-#                 ft.drive(80, 95, 1)
-#                 time.sleep(0.01)
-        
-#         elif mode == 1: # blue
-#             if by > 300:
-#                 ft.turnTimer(0.8, [80, 95, 1])
-#                 ft.turnTimer(0.8, [80, 115, 1])
-#                 print(f'Passed {numLines}, mode = {mode}')
-#                 numLines += 1
-#             elif not ft.sensorDetect():
-#                 ft.drive(80, 95, 1)
-#                 time.sleep(0.01)
-
-#         elif mode == 2: 
-#             if oy > 300:
-#                 ft.turnTimer(0.8, [80, 95, 1])
-#                 ft.turnTimer(0.8, [80, 65, 1])
-#                 print(f'Passed {numLines}, mode = {mode}')
-#                 numLines += 1
-#             elif not ft.sensorDetect(invert=True):
-#                 ft.drive(80, 95, 1)
-#                 time.sleep(0.01)
-    
-#     delay = time.time()
-#     while time.time() - delay < 1:
-#         if mode == 2:
-#             if not ft.sensorDetect(invert=True):
-#                 ft.drive(80, 95, 1)
-#                 time.sleep(0.01)
-#         elif not ft.sensorDetect():
-#             ft.drive(80, 95, 1)
-#             time.sleep(0.01)
-
-#     print(f'[INFO] Total time: {time.time() - timeIt:.2f}s')
 
 try:
-    run = True
-    while run:
-        print('\n[INFO] Running...')
-        main()
-        delay = time.time()
-        while time.time() - delay < 3:
-            if not ft.getBtState():
-                run = False
-                break
+    pass
 
 except KeyboardInterrupt:
     pass
-    #cv2.imwrite('a.jpg', frame)
 except Exception as e:
     print(e)
 
 print('[INFO] Exiting...')
 ft.shutdown()
-cam.shutdown()
--- tests/servo.py
+++ tests/servo.py
@@ -3,13 +3,33 @@ from gpiozero import AngularServo
 from time import sleep
 
 pigpio_factory = PiGPIOFactory()
-servo =AngularServo(7, min_angle=0, max_angle=270, min_pulse_width=0.0005, max_pulse_width=0.0025, pin_factory=pigpio_factory)
-servo.angle = 180
-sleep(2)
+servo =AngularServo(7, min_angle=0, max_angle=180, min_pulse_width=0.0005, max_pulse_width=0.0025, pin_factory=pigpio_factory)
+
 servo.angle = 90
-sleep(2)
-servo.angle = 135
-sleep(2)
-# servo.angle = 95
+sleep(1)
+
+## Steering servo ##
+
+# servo.angle = 50 # Left
+# sleep(1)
+# servo.angle = 95 # Straight
+# sleep(2)
+# servo.angle = 130 # Right
 # sleep(2)
 
+## Steering servo ##
+
+## Camera servo ##
+
+# servo.angle = 0 # Left
+# sleep(1)
+# servo.angle = 45 # Slightly-Left
+# sleep(1)
+# servo.angle = 82 # Straight
+# sleep(2)
+# servo.angle = 130 # Slightly-Right
+# sleep(1)
+# servo.angle = 170 # Right
+# sleep(2)
+
+## Camera servo ##
